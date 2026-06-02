aa0fc84d7c2316386890cd7e947239cbd9bb5a10
Practicing <gargvanshika662@gmail.com>
2026-06-02 15:27:43 +0530

    adding more lines of code

--- logic.js
+++ logic.js
@@ -18,3 +18,26 @@ for(let item of li){
     p.innerText=i;
   });
 }
+let content=document.querySelector(".list-content");
+let item=document.querySelector(".add_items");
+item.addEventListener("click",()=>{
+      let text = document.querySelector("#text").value;
+       let text2=p.innerText;
+       let text3=document.querySelector("#due").value;
+       let now = new Date().toLocaleString("en-IN", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit"
+});
+       if(text2=="Select Priority"){
+            text2="";
+       }
+ content.innerHTML+=`<li>   <i class="fa-solid fa-trash"></i><br><b>${text}</b></li> <p style="font-size:14px ;color:#262626;font-weight:600"><i>Created on:&nbsp;${now}</i></p>
+ <p style="font-size:14px ;color:#262626; font-weight:600"><i>Due Date:</i>&nbsp;<span>${text3}</span></p>
+ <p style="color:grey;font-size:14px;font-weight:600"><i>${text2}</i></p><br>`;
+
+  content.style.display="block";
+    }
+  );
