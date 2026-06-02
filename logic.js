30872c70e25b012ee3fd0facb49568f42a5b94c0
Practicing <gargvanshika662@gmail.com>
2026-06-02 17:54:40 +0530

    added more code

--- logic.js
+++ logic.js
@@ -18,7 +18,8 @@ for(let item of li){
     p.innerText=i;
   });
 }
-let content=document.querySelector(".list-content");
+let content=document.querySelector(".list-text");
+let content1=document.querySelector(".list-content");
 let item=document.querySelector(".add_items");
 item.addEventListener("click",()=>{
       let text = document.querySelector("#text").value;
@@ -34,10 +35,18 @@ item.addEventListener("click",()=>{
        if(text2=="Select Priority"){
             text2="";
        }
- content.innerHTML+=`<li>   <i class="fa-solid fa-trash"></i><br><b>${text}</b></li> <p style="font-size:14px ;color:#262626;font-weight:600"><i>Created on:&nbsp;${now}</i></p>
- <p style="font-size:14px ;color:#262626; font-weight:600"><i>Due Date:</i>&nbsp;<span>${text3}</span></p>
- <p style="color:grey;font-size:14px;font-weight:600"><i>${text2}</i></p><br>`;
+ content1.innerHTML+=`<li><i class="fa-solid fa-trash" onclick="del(this)"></i><br><b>${text}</b> <p style="font-size:12px ;color:#262626;font-weight:600"><i>Created on:&nbsp;${now}</i></p>
+ <p style="font-size:12px ;color:#262626; font-weight:600"><i>Due Date:</i>&nbsp;<span>${text3}</span></p>
+ <p style="color:grey;font-size:12px;font-weight:600"><i>${text2}</i></p><br></li>`;
 
   content.style.display="block";
-    }
+    }               /*onclick="del(this) */
   );
+function del(element){
+    element.parentElement.remove();
+}
+
+let closing=document.querySelector(".close");
+closing.addEventListener("click",()=>{
+   content.style.display="none";
+});
